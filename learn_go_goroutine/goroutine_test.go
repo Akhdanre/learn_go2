22871217e39349a890377dce579daa68b2a5a5bb
Akeon <danakhdan12@gmail.com>
2024-01-23 00:00:13 +0700

    testing many goroutine to confirm the process is very light

--- learn_go_goroutine/goroutine_test.go
+++ learn_go_goroutine/goroutine_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 	"time"
 )
+func DisplayNumber(number int){
+	fmt.Println("Display", number)
+}
 
 func RunHelloWordl() {
 	fmt.Println("Hello world")
@@ -15,4 +18,11 @@ func TestCreateGoroutine(t *testing.T) {
 
 	fmt.Println("Ups")
 	time.Sleep( 1 * time.Second)
+}
+
+func TestManyGoroutine(t *testing.T){
+	for i:= 0 ; i < 100000; i++{
+		 go DisplayNumber(i)
+	}
+	time.Sleep(5 * time.Second)
 }
