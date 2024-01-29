05fb3949f86e195669b5da73bc10c5d06c50e1e6
Akeon <danakhdan12@gmail.com>
2024-01-29 13:26:30 +0700

    channel in and out

--- learn_go_goroutine/channel_test.go
+++ learn_go_goroutine/channel_test.go
@@ -6,10 +6,9 @@ import (
 	"time"
 )
 
-func TestCreateChannel(t *testing.T){
+func TestCreateChannel(t *testing.T) {
 	channel := make(chan string)
 	defer close(channel)
-
 
 	// channel <- "akhdan"
 
@@ -23,31 +22,52 @@ func TestCreateChannel(t *testing.T){
 
 	// fmt.Println(data)
 
-
-	go func(){
+	go func() {
 		time.Sleep(2 * time.Second)
 		channel <- "Akhdan Robbani"
 		fmt.Println("selesai mengirim data ke channel")
 	}()
-	
-	data := <- channel
+
+	data := <-channel
 	fmt.Println(data)
 	time.Sleep(5 * time.Second)
 
 }
 
-func GiveMeResponse(channel chan string){
+func GiveMeResponse(channel chan string) {
 	time.Sleep(2 * time.Second)
 	channel <- "Akhdan Robbani"
 }
 
-func TestChannnelAsParameter(t *testing.T){
+func TestChannnelAsParameter(t *testing.T) {
 	channel := make(chan string)
 	defer close(channel)
 
 	go GiveMeResponse(channel)
-	
-	data := <- channel
+
+	data := <-channel
 	fmt.Println(data)
 	time.Sleep(5 * time.Second)
+}
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "test only in"
+	//data := <- channel // will error cause channel only in 
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <- channel
+	fmt.Println(data)
+}
+
+func TestInAndOut(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(2 * time.Second)
+	
 }
