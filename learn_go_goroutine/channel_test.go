42a0cf08b496e2c3c8990a7c5f79d7e612031594
Akeon <danakhdan12@gmail.com>
2024-01-30 20:40:47 +0700

    channel select

--- learn_go_goroutine/channel_test.go
+++ learn_go_goroutine/channel_test.go
@@ -95,12 +95,11 @@ func TestBufferedChannel(t *testing.T) {
 
 }
 
-
-func TestRangeChannel(t *testing.T){
+func TestRangeChannel(t *testing.T) {
 	channel := make(chan string)
-	
-	go func(){
-		for i := 0; i < 10; i++{
+
+	go func() {
+		for i := 0; i < 10; i++ {
 			channel <- "perulanga ke " + strconv.Itoa(i)
 			time.Sleep(2 * time.Second)
 		}
@@ -108,9 +107,36 @@ func TestRangeChannel(t *testing.T){
 
 	}()
 
-	for data := range channel{
+	for data := range channel {
 		fmt.Println(data)
 	}
 
 	fmt.Println("selesai")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+	go GiveMeResponse(channel1)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("data dari channel1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("data dari channel2", data)
+			counter++
+		}
+		if counter == 3 {
+			break
+		}
+	}
 }
