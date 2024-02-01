bcc31fd304166aaaab2ac3a0b276dcbb3359c9bd
Akeon <danakhdan12@gmail.com>
2024-02-01 19:55:19 +0700

    adjust

--- learn_go_goroutine/channel_test.go
+++ learn_go_goroutine/channel_test.go
@@ -134,6 +134,33 @@ func TestSelectChannel(t *testing.T) {
 		case data := <-channel2:
 			fmt.Println("data dari channel2", data)
 			counter++
+		}
+
+		if counter == 3 {
+			break
+		}
+	}
+}
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+	go GiveMeResponse(channel1)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("data dari channel1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("data dari channel2", data)
+			counter++
 		default:
 			fmt.Println("menunggu data")
 		}
