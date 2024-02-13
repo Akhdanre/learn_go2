e67aef3dfd7ece13ad0b0db5fbdf966f3e787b73
Akeon <danakhdan12@gmail.com>
2024-02-13 13:29:24 +0700

    learn scan data with data type

--- learn_go_database/sql_test.go
+++ learn_go_database/sql_test.go
@@ -2,12 +2,13 @@ package learngodatabase
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
-
-func TestExecSql(t *testing.T){
+func TestExecSql(t *testing.T) {
 	db := GetConnection()
 	defer db.Close()
 
@@ -19,4 +20,69 @@ func TestExecSql(t *testing.T){
 		panic(err)
 	}
 	fmt.Println("succes insert new customer")
+}
+
+func TestQuerySelect(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := "SELECT id, name FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("id :", id, "name :", name)
+	}
+}
+
+func TestQuerySQLComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := "SELECT id, name, email, balance, rating, birth_data, married, created_at FROM customer"
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString
+		var balance int32
+		var rating float32
+		var birhtDate sql.NullTime
+		var createAt time.Time
+		var married bool
+
+		err = rows.Scan(&id, &name, &email, &balance, &rating, &birhtDate, &married, &createAt)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("id :", id)
+		fmt.Println("name :", name)
+		if email.Valid {
+			fmt.Println("email :", email)
+		}
+		fmt.Println("balance :", balance)
+		fmt.Println("rating :", rating)
+		if birhtDate.Valid {
+			fmt.Println("birthdate :", birhtDate)
+		}
+		fmt.Println("married :", married)
+		fmt.Println("create at :", createAt)
+		fmt.Println("=====================")
+	}
 }
