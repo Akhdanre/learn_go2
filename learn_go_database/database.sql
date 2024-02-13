b0a5a458e8a3091488eb025aad85da6637c78815
Akeon <danakhdan12@gmail.com>
2024-02-13 13:28:48 +0700

    sql query for learn go database

--- learn_go_database/database.sql
+++ learn_go_database/database.sql
@@ -1,5 +1,89 @@
-
 create table customer (
     id varchar(100) not null primary key,
     name varchar(100) not null
-)
+);
+alter table customer
+add column email varchar(100);
+alter table customer
+add column balance int DEFAULT 0,
+    add column rating double default 0.0,
+    add column created_at timestamp default CURRENT_TIMESTAMP,
+    add column birth_data DATE;
+alter table customer
+add married boolean default false;
+desc customer;
+insert into customer (
+        id,
+        name,
+        email,
+        balance,
+        rating,
+        birth_data,
+        married
+    )
+values (
+        'dani',
+        'dani',
+        'dani@gmail.com',
+        1000000,
+        90.0,
+        '2003-08-12',
+        false
+    );
+insert into customer (
+        id,
+        name,
+        email,
+        balance,
+        rating,
+        birth_data,
+        married
+    )
+values (
+        'oukenzeumasio',
+        'oukenzuemasio',
+        'oukenzuemasio@gmail.com',
+        1000000,
+        90.0,
+        '2004-05-12',
+        false
+    ),
+    (
+        'akrida',
+        'akrida',
+        'akrida@gmail.com',
+        1000000,
+        90.0,
+        '2004-05-18',
+        false
+    );
+select id,
+    name,
+    email,
+    balance,
+    rating,
+    birth_data,
+    married,
+    created_at
+from customer;
+delete from customer
+where id = 'robbani';
+
+insert into customer (
+        id,
+        name,
+        email,
+        balance,
+        rating,
+        birth_data,
+        married
+    )
+values (
+        'lofta',
+        'lofta',
+        NULL,
+        1000000,
+        90.0,
+        NULL,
+        true
+    );
